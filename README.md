d8dc275ae752fb0920f90cffc11a9972184e5ae8
Hyunjin-Kim <khj94811@naver.com>
2021-01-06 15:30:23 +0900

    Add initial project
    Signed-off-by: Hyunjin-Kim <khj94811@naver.com>

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # JUnitTest-practice
+
+- JUnit4 Test 해보기
+- Spring Container 를 이용해서 JUnit Test 해보기
---
+++ src/test/java/JUnitSetTest.java
@@ -0,0 +1,30 @@
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class JUnitSetTest {
+    static Set<JUnitSetTest> testObjects = new HashSet<JUnitSetTest>();
+
+    @Test
+    public void test1() {
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
+    }
+
+    @Test
+    public void test2() {
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
+    }
+
+    @Test
+    public void test3() {
+        assertThat(testObjects, not(hasItem(this)));
+        testObjects.add(this);
+    }
+}
